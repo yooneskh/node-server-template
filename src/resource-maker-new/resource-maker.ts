d90f8bcf124a46dd903e2c6057a750669a9d1692
Yoones Khoshghadam <yoonskh@gmail.com>
2019-10-28 01:29:59 +0330

    finalize new resource maker controller

--- src/resource-maker-new/resource-maker.ts
+++ src/resource-maker-new/resource-maker.ts
@@ -3,6 +3,8 @@ import { Model } from 'mongoose';
 import { makeMainResourceModel, makeResourceRelationModel } from './resource-model';
 import { ServerError } from '../global/errors';
 import { ResourceController } from './resource-controller';
+import { ResourceRelationController } from './resource-relation-controller';
+import { Router } from 'express';
 
 export class ResourceMaker<T extends IResource> {
 
@@ -13,6 +15,11 @@ export class ResourceMaker<T extends IResource> {
 
   private resourceModel?: Model<T>;
   private resourceRelationModels: Model<IResource>[] = [];
+
+  private resourceController?: ResourceController<T>;
+  private resourceRelationControllers: ResourceRelationController<IResource>[] = [];
+
+  private resourceRouter?: Router;
 
   constructor(name: string) {
     this.resourceName = name;
@@ -37,10 +44,15 @@ export class ResourceMaker<T extends IResource> {
     this.resourceRelations.push(relation);
 
     const relationModel = makeResourceRelationModel<P>(this.resourceName, relation);
-
     this.resourceRelationModels.push(relationModel);
 
-    return relationModel;
+    const relationController = new ResourceRelationController<P>(this.resourceName, relation.targetModelName, relationModel, relation);
+    this.resourceRelationControllers.push(relationController);
+
+    return {
+      model: relationModel,
+      controller: relationController
+    };
 
   }
 
@@ -64,10 +76,37 @@ export class ResourceMaker<T extends IResource> {
       throw new ServerError('model not made for ' + this.resourceName);
     }
 
-    const controller = new ResourceController<T>(this.resourceModel, this.resourceProperties);
+    this.resourceController = new ResourceController<T>(this.resourceModel, this.resourceProperties);
 
-    // TODO: here ...
+    return this.resourceController;
 
+  }
+
+  public getRelationControllers() {
+    return this.resourceRelationControllers;
+  }
+
+  public getRouter() {
+
+    // this.resourceRouter = 'adsfasdf'; // TODO: this is next step!
+
+    return this.resourceRouter;
+
+  }
+
+  public getMC() {
+    return {
+      model: this.getModel(),
+      controller: this.getController()
+    }
+  }
+
+  public getMCR() {
+    return {
+      model: this.getModel(),
+      controller: this.getController(),
+      router: this.getRouter()
+    }
   }
 
 }
