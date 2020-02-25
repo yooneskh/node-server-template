ffcfa9433b3c5966e6f41b4f9d7128d0fd2007fd
Yoones Khoshghadam <yooneskh@gmail.com>
2020-02-25 21:50:04 +0330

    finalize resource model

--- src/plugins/resource-maker-next/resource-model-types.d.ts
+++ src/plugins/resource-maker-next/resource-model-types.d.ts
@@ -1,4 +1,11 @@
 export interface ResourceModelProperty {
   key: string;
   type: string;
+  ref?: string;
+  default?: any;
+  required?: boolean;
+  unique?: boolean;
+  select?: boolean;
+  isArray?: boolean;
+  languages?: Record<string, any>;
 }
--- src/plugins/resource-maker-next/resource-model.ts
+++ src/plugins/resource-maker-next/resource-model.ts
@@ -1,11 +1,83 @@
 import { ResourceModelProperty } from './resource-model-types';
 import { ServerError } from '../../global/errors';
+import { Schema, model, Document } from 'mongoose';
 
-export class ResourceModel {
+// tslint:disable: no-any
+export class ResourceModel<T> {
 
   private properties: ResourceModelProperty[] = [];
 
   constructor(private name: string) { }
+
+  private mapPropertyTypeToMongooseType(propertyType: string): any {
+    switch (propertyType) {
+      case 'string': return String;
+      case 'number': return Number;
+      case 'boolean': return Boolean;
+      case 'object': return Object;
+      default: throw new ServerError(`resource property type unknown: ${propertyType}`);
+    }
+  }
+
+  private makeSchemaOptions() {
+
+    const schemaOptions: Record<string, any> = {};
+
+    for (const property of this.properties) {
+
+      let scheme: Record<string, any> = {};
+
+      scheme.type = this.mapPropertyTypeToMongooseType(property.type)
+
+      if (property.ref !== undefined) scheme.ref = property.ref;
+      if (property.default !== undefined) scheme.default = property.default;
+      if (property.required !== undefined) scheme.required = property.required;
+      if (property.unique !== undefined) scheme.unique = property.unique;
+      if (property.select !== undefined) scheme.select = property.select;
+
+      if (property.languages) {
+
+        const newScheme: Record<string, any> = {};
+
+        for (const language in property.languages) {
+          newScheme[language] = { ...scheme, ...property.languages[language] };
+        }
+
+        scheme = newScheme;
+
+      }
+
+      schemaOptions[property.key] = property.isArray ? [scheme] : scheme;
+
+    }
+
+    schemaOptions['__v'] = {
+      type: Number,
+      select: false
+    };
+
+    schemaOptions['createdAt'] = {
+      type: Number,
+      default: Date.now
+    };
+
+    schemaOptions['updatedAt'] = {
+      type: Number,
+      default: 0
+    };
+
+    return schemaOptions;
+
+  }
+
+  private makeSchema() {
+    return new Schema(
+      this.makeSchemaOptions(),
+      {
+        minimize: false
+      }
+    );
+  }
 
   public addProperty(property: ResourceModelProperty) {
 
@@ -15,12 +87,11 @@ export class ResourceModel {
 
   }
 
-  private makeSchema() {
-
-  }
-
   public getModel() {
-
+    return model<T & Document>(
+      this.name,
+      this.makeSchema()
+    );
   }
 
 }
