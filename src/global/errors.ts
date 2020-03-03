8b8b676ad640242622f1359118fb93ec7f6f6007
Yoones Khoshghadam <yooneskh@gmail.com>
2020-03-03 15:08:18 +0330

    refactor errors

--- src/global/errors.ts
+++ src/global/errors.ts
@@ -34,8 +34,11 @@ export function errorHandler(error: Error, request: Request, response: Response,
   else if (error instanceof ForbiddenAccessError) {
     response.status(403).json({ code: error.code, message: error.message});
   }
+  else if (error instanceof HandleableError) {
+    response.status(403).json({ code: error.code, message: error.message});
+  }
   else {
-    response.status(400).json({ code: error instanceof HandleableError ? error.code : -1, message: error.message});
+    response.status(400).json({ code: -1, message: error.message});
   }
 
 }
