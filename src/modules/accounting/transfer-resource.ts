5187ce32d4c9843c46a9c2993119cbf4942be27a
Yoones Khoshghadam <yooneskh@gmail.com>
2020-09-09 00:08:25 +0430

    better transfer descriptions

--- src/modules/accounting/transfer-resource.ts
+++ src/modules/accounting/transfer-resource.ts
@@ -110,11 +110,11 @@ export async function createTransfer(fromAccountId: string, toAccountId: string,
     }
   });
 
-  const fromTransaction = await createTransaction(fromAccountId, -amount, description);
+  const fromTransaction = await createTransaction(fromAccountId, -amount, `برداشت جهت ${description}`);
   transfer.fromTransaction = fromTransaction._id;
   await transfer.save();
 
-  const toTransaction = await createTransaction(toAccountId, amount, description);
+  const toTransaction = await createTransaction(toAccountId, amount, `واریز جهت ${description}`);
   transfer.toTransaction = toTransaction._id;
   await transfer.save();
 
