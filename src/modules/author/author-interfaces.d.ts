7a4fbfdc547d5ea2c9f090e1bb27b3bcbd1123d2
Yoones Khoshghadam <yooneskh@gmail.com>
2020-10-15 23:12:05 +0330

    fix author relation types

--- src/modules/author/author-interfaces.d.ts
+++ src/modules/author/author-interfaces.d.ts
@@ -6,12 +6,16 @@ export interface IAuthorBase extends IResource {
 } export interface IAuthor extends IAuthorBase, Document {}
 
 export interface IAuthorBookBase extends IResource {
+  author: string;
+  book: string;
   timeTook?: number;
   blast?: string;
   pages?: string[];
 } export interface IAuthorBook extends IAuthorBookBase, Document {}
 
 export interface IAuthorPageMakerBase extends IResource {
+  author: string;
+  page: string;
   isPremium?: number;
   contributor?: string;
 } export interface IAuthorPageMaker extends IAuthorPageMakerBase, Document {}
