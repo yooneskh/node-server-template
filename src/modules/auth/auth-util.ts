37e871d6a94b4f4b8bf72d8cd117bb70d34d938c
Yoones Khoshghadam <yooneskh@gmail.com>
2021-07-03 00:07:23 +0430

    :D

--- src/modules/auth/auth-util.ts
+++ src/modules/auth/auth-util.ts
@@ -28,7 +28,7 @@ export const PERMISSIONS = [
         ['publisher', 'list', 'list-count', 'retrieve', 'create', 'update', 'delete'],
         ['api-service', 'list', 'list-count', 'retrieve', 'create', 'update', 'delete'],
         ['api-endpoint', 'list', 'list-count', 'retrieve', 'create', 'update', 'delete'],
-        ['api-version', 'list', 'list-count', 'retrieve', 'create', 'update', 'delete']
+        ['api-version', 'list', 'list-count', 'retrieve', 'create', 'update', 'delete', 'run']
       ],
       ...[ // others
 
@@ -78,7 +78,7 @@ export const PERMISSIONS_LOCALES = {
       'api-version': 'نسخه Api'
     },
     ...{ // actions
-
+      'run': 'اجرا'
     },
     ...{ // others
 
