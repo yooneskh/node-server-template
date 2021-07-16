dbd344c0bbeadd4c2a34a28b4f827d483d2a59ef
Yoones Khoshghadam <yooneskh@gmail.com>
2021-07-17 02:08:23 +0430

    feat (errors): response headers

--- src/global/errors.ts
+++ src/global/errors.ts
@@ -4,6 +4,7 @@ export class HandleableError extends Error {
 
   public code = 1000;
   public statusCode = 400;
+  public responseHeaders: Record<string, unknown> = {};
 
   // tslint:disable-next-line: no-any
   constructor(message?: string | undefined, public responseMessage?: string | undefined, public extra?: Record<string, any> | undefined) {
@@ -39,14 +40,19 @@ export class RouteBypassedError extends HandleableError {
 }
 
 export function errorHandler(error: Error, _request: Request, response: Response, _next: Function) {
+  if (error instanceof RouteBypassedError) return;
 
-  if (!(error instanceof RouteBypassedError)) console.error('Error ::', error.message);
+  console.error('Error ::', error.message);
 
-  if (error instanceof RouteBypassedError) {
-    // noop
-  }
-  else if (error instanceof HandleableError) {
-    response.status(error.statusCode).json({ code: error.code, message: error.responseMessage ?? error.message, ...(error.extra || {}) });
+  if (error instanceof HandleableError) {
+
+    for (const header in error.responseHeaders) {
+      response.setHeader(header, String(error.responseHeaders[header]));
+    }
+
+    response.status(error.statusCode)
+    response.json({ code: error.code, message: error.responseMessage ?? error.message, ...(error.extra || {}) });
+
   }
   else {
     response.status(400).json({ message: error.message });
