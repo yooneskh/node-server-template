be7e7175cb762345cea7f9be14da7c3b6e17be7f
Yoones Khoshghadam <yooneskh@gmail.com>
2021-07-17 11:55:13 +0430

    :D

--- src/modules/api/runner/api-runner.ts
+++ src/modules/api/runner/api-runner.ts
@@ -47,6 +47,8 @@ export async function runApi(permit: IApiPermit, api: IApiVersion, payload?: IAp
         }
       });
 
+      console.log({ f: JSON.stringify(policyResult, undefined, '  ') });
+
       policyResult.error!.responseHeaders = policyResult.headers;
       throw policyResult.error;
 
