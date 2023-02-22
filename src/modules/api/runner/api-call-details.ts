727744665f78a23f7a7510747eb3db90f7dd1ca4
Yoones Khoshghadam <yooneskh@gmail.com>
2023-02-22 16:47:15 +0330

    feat (api stats): improve structure

--- src/modules/api/runner/api-call-details.ts
+++ src/modules/api/runner/api-call-details.ts
@@ -1,5 +1,7 @@
+import { InvalidRequestError } from '../../../global/errors';
 import { ResourceMaker } from '../../../plugins/resource-maker/resource-maker';
 import { ApiLogController } from '../api-log-resource';
+import { ApiPermitController } from '../api-permit-resource';
 
 
 const maker = new ResourceMaker('ApiCallDetails');
@@ -9,32 +11,67 @@ maker.addAction({
   method: 'POST',
   path: '/api/api-details',
   signal: ['Route', 'ApiCallDetails', 'Execute'],
-  rateLimitOptions: undefined,
-  dataProvider: async ({payload}) => {
+  rateLimitOptions: {
+    pointsAmount: 3,
+    pointsInterval: 60 * 1,
+    blockDuration: 60 * 2,
+    consecutiveFailDurationMultiplier: 1.5,
+  },
+  dataProvider: async ({ payload }) => {
 
-   const successfullCallCount = await ApiLogController.count({
-    filters: {
-      success: true,
-      $and: [
-        {
-          createdAt: { $gte: payload.startAt }
-        },
-        {
-          createdAt: { $lte: payload.endAt }
-        }
-      ]
-    },
-    skipKeyCheck: true
-  });
+    const { user, permit } = payload;
 
-  return [
-    {
-      'icon': 'mdi-check-circle',
-      'title': 'تعداد فراخوانی‌های موفق Api',
-      'value': successfullCallCount,
-      'width': 4
+
+    const query: any = {};
+
+    if (permit) {
+      query['permit'] = permit;
     }
-  ];
+    else if (user) {
+
+      const userPermits = await ApiPermitController.list({
+        filters: {
+          user,
+        }
+      });
+
+      query['permit'] = { $in: userPermits.map(it => String(it._id)) };
+
+    }
+    else {
+      throw new InvalidRequestError('مجوز یا کاربر باید ارسال شود.');
+    }
+
+
+    const logs = await ApiLogController.list({
+      filters: query,
+      skipKeyCheck: true,
+    });
+
+
+    return [
+      {
+        type: 'banner-icon',
+        icon: 'mdi-earth',
+        title: 'تعداد فراخوانی‌های Api',
+        value: logs.length,
+        width: 4,
+      },
+      {
+        type: 'banner-icon',
+        icon: 'mdi-check-circle',
+        title: 'تعداد فراخوانی‌های موفق Api',
+        value: logs.filter(it => it.success).length,
+        width: 4,
+      },
+      {
+        type: 'banner-icon',
+        icon: 'mdi-close-circle',
+        title: 'تعداد فراخوانی‌های ناموفق Api',
+        value: logs.filter(it => !it.success).length,
+        width: 4,
+      },
+    ];
 
 }
 });
